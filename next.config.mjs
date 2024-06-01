dd5d748b72ad909dc6555fb3a373bd4d024134d3
Sarthak0203 <138149235+Sarthak0203@users.noreply.github.com>
2024-06-02 00:27:40 +0530

    Configure for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-};
-
+    basePath: '/your-repo-name',
+    assetPrefix: '/your-repo-name/',
+    images: {
+      loader: 'akamai',
+      path: '',
+    },
+    trailingSlash: true,
+  };
+  
+  module.exports = nextConfig;
