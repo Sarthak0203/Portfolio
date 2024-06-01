74c90cc6ec5d0a6b133d9351577c6f2993dca26e
Sarthak0203 <138149235+Sarthak0203@users.noreply.github.com>
2024-06-02 01:45:10 +0530

    some changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    distDir: "build",
+};
 
 export default withSentryConfig(nextConfig, {
 // For all available options, see:
