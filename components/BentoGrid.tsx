a2a6275d717b1bfa53fd06f35444c77acbe77c56
Sarthak0203 <138149235+Sarthak0203@users.noreply.github.com>
2024-06-03 23:12:14 +0530

    Updated Skills

--- components/BentoGrid.tsx
+++ components/BentoGrid.tsx
@@ -53,8 +53,8 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const leftLists = ["ReactJS", "Express", "Javascript"];
+  const rightLists = ["nextJS", "MongoDB", "NodeJS"];
   const [copied, setCopied] = useState(false);
 
   const defaultOptions = {
